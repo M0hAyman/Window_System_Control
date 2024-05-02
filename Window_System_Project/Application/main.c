afc3266ba344d03b905105ad28207c241f024bf9
DevYehia <yehiayes20@gmail.com>
2024-05-03 01:11:24 +0300

    Added Passenger Task and fixed indentation

--- Window_System_Project/Application/main.c
+++ Window_System_Project/Application/main.c
@@ -16,45 +16,79 @@ QueueHandle_t driverQueue;
 QueueHandle_t passengerQueue;
 xSemaphoreHandle xMutex;
 uint8 motorRunning = 0;
+
 void driverTask(void *params){
-int command;
-while(1){
-xQueueReceive(driverQueue,&command,portMAX_DELAY); 
-xSemaphoreTake(xMutex,portMAX_DELAY);
-xSemaphoreGive(xMutex);
-if(command == 0 && !motorRunning){
-motorRunning = 1;
-MOTOR_rotate(PORT_F,PIN1,PORT_F,PIN2);
-vTaskDelay(200);
-	if (DIO_ReadPin(PORT_D,PIN0)==0){
-		while(DIO_ReadPin(PORT_D,PIN0)==0);
-		MOTOR_stop(PORT_F,PIN1,PORT_F,PIN2);
-	
-	}
-	
+    int command;
+    while(1){
+        xQueueReceive(driverQueue,&command,portMAX_DELAY);  //Wait For command
+        xSemaphoreTake(xMutex,portMAX_DELAY); //check if there is a jamming emergency
+        xSemaphoreGive(xMutex);
+        if(command == UP && !motorRunning){ 
+            motorRunning = 1;
+            MOTOR_rotate(PORT_F,PIN1,PORT_F,PIN2);
+            vTaskDelay(200);
+                if (DIO_ReadPin(PORT_D,PIN0)==0){
+                    while(DIO_ReadPin(PORT_D,PIN0)==0);
+                    MOTOR_stop(PORT_F,PIN1,PORT_F,PIN2);
+                
+                }        
+        }
+
+        else if(command == DOWN && !motorRunning){
+            motorRunning = 1;
+            MOTOR_rotateOpposite(PORT_F,PIN1,PORT_F,PIN2);
+            vTaskDelay(200);
+                if (DIO_ReadPin(PORT_D,PIN1)==0){
+                    while(DIO_ReadPin(PORT_D,PIN1)==0);
+                    MOTOR_stop(PORT_F,PIN1,PORT_F,PIN2);
+                }
+                
+        }
+        else{ //Second press in auto mode turns the motor off
+            MOTOR_stop(PORT_F,PIN1,PORT_F,PIN2);	
+            motorRunning = 0;
+        }
+
+    }
 }
 
-else if(command == 1 && !motorRunning){
-motorRunning = 1;
-MOTOR_rotateOpposite(PORT_F,PIN1,PORT_F,PIN2);
-vTaskDelay(200);
-	if (DIO_ReadPin(PORT_D,PIN1)==0){
-		while(DIO_ReadPin(PORT_D,PIN1)==0);
-		MOTOR_stop(PORT_F,PIN1,PORT_F,PIN2);
-	
-	}
-		
-	
-}
-else{
-		MOTOR_stop(PORT_F,PIN1,PORT_F,PIN2);	
-		motorRunning = 0;
-}
+void passengerTask(void *params){
+    int command;
+    while(1){
+        xQueueReceive(driverQueue,&command,portMAX_DELAY); 
+        xSemaphoreTake(xMutex,portMAX_DELAY);
+        xSemaphoreGive(xMutex);
+        if(command == 0 && !motorRunning){
+        motorRunning = 1;
+        MOTOR_rotate(PORT_F,PIN1,PORT_F,PIN2);
+        vTaskDelay(200);
+            if (DIO_ReadPin(PORT_D,PIN0)==0){
+                while(DIO_ReadPin(PORT_D,PIN0)==0);
+                MOTOR_stop(PORT_F,PIN1,PORT_F,PIN2);
+            
+            }
+            
+        }
 
-}
+        else if(command == 1 && !motorRunning){
+        motorRunning = 1;
+        MOTOR_rotateOpposite(PORT_F,PIN1,PORT_F,PIN2);
+        vTaskDelay(200);
+            if (DIO_ReadPin(PORT_D,PIN1)==0){
+                while(DIO_ReadPin(PORT_D,PIN1)==0);
+                MOTOR_stop(PORT_F,PIN1,PORT_F,PIN2);
+            
+            }
+                
+            
+        }
+        else{
+                MOTOR_stop(PORT_F,PIN1,PORT_F,PIN2);	
+                motorRunning = 0;
+        }
+    }
 }
 void initTask(){
-	
 	DIO_Init(PORT_D);
 	MOTOR_direction_init(PORT_F,PIN1,PORT_F,PIN2);
 	DIO_SetupDirection(PORT_D,IN,PIN0);
@@ -63,9 +97,6 @@ void initTask(){
 	interrupt_init(GPIOD,FALLING_EDGE,PIN1);
 	interrupt_enable_pin(GPIOD,PIN0);
 	interrupt_enable_pin(GPIOD,PIN1);
-	
-
-	
 }
 
 // driver isr 
@@ -89,10 +120,9 @@ int main()
 {	initTask();
 	driverQueue = xQueueCreate(1, sizeof(long));
 	xMutex = xSemaphoreCreateMutex();
-	xTaskCreate(driverTask,"ELSOA2",100,0,1,0);
+	xTaskCreate(driverTask,"ELSOA2",100,0,2,0);
+	xTaskCreate(passengerTask,"ELLY GNB EL SOA2",100,0,1,0);
 	vTaskStartScheduler();
-//    passengerQueue = xQueueCreate(1, sizeof(uint8));
 
-// 
 	for(;;);
 }
